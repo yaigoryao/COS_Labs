ae643e7dda45165de5ccebdeec652845fa0f9e8c
yaigoryao <petrochenkov.igor2015@yandex.ru>
2025-10-10 20:21:36 +0300

    Загружены файлы и отчеты домашних работ 1, 2

---
+++ hw1/lr6/lr6/lr6.py
@@ -0,0 +1,203 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+from numpy.fft import fftfreq
+import scipy.signal as sgl
+import scipy.fft as fft
+
+A1 = 0.5
+A2 = 2.0
+A3 = 1.7
+A4 = 2.5
+
+omega1 = 90
+omega2 = 30
+omega3 = 30
+omega4 = 150
+
+phi1 = 0
+phi2 = 30
+phi3 = -60
+phi4 = 60
+
+func = lambda A, omega, phi: (lambda x: A * np.sin(2 * np.pi * x * omega + phi))
+
+s1 = func(A1, omega1, phi1)
+s2 = func(A2, omega2, phi2)
+s3 = func(A3, omega3, phi3)
+s4 = func(A4, omega4, phi4)
+
+fs = 16000
+t = 1.0 / fs
+
+T = 0.1
+N = int(fs * T)
+
+x = np.linspace(0, T, N)
+
+y1 = s1(x)
+y2 = s2(x)
+y3 = s3(x)
+y4 = s4(x)
+
+y = (y1 + y2 + y3) * y4
+
+yf = fft.fft(y)
+yf_full = np.array(yf)
+xf_full = fft.fftfreq(N, t)
+
+yf = (2.0 / N) * np.abs(yf[0:N//2])
+xf = xf_full[:N//2]
+
+harmonics_freq_idx = sgl.argrelextrema(yf, np.greater)[0]
+harmonics_freq_vals = xf[harmonics_freq_idx]
+
+#freq_delta = 3.0
+#Rp = 0.1
+#Rs = 40
+#n = 5
+
+#clamp = lambda x, mn, mx: int(mn) if x < mn else int(mx) if x > mx else int(x)
+#clamp_yf = lambda x: clamp(x, 0, yf_full.size)
+
+#epsilon = 3
+
+def extract_harmonic(freq, bandwidth=3.0):
+    mask_pos = ((xf_full >= freq - bandwidth) & (xf_full <= freq + bandwidth))
+    mask_neg = ((xf_full >= -freq - bandwidth) & (xf_full <= -freq + bandwidth))
+    
+    yf_filtered = np.zeros_like(yf_full)
+    yf_filtered[mask_pos] = yf_full[mask_pos]
+    yf_filtered[mask_neg] = yf_full[mask_neg]
+    
+    return yf_filtered
+#filter_func = lambda freq: sgl.filtfilt(*sgl.ellip(n, Rp, Rs, [freq - freq_delta, freq + freq_delta], 'bandpass', fs=fs), y)
+#filter_func = lambda freq_idx: np.pad(yf_cplx[clamp_yf(freq_idx-epsilon):clamp_yf(freq_idx+epsilon)], (clamp_yf(freq_idx-epsilon), yf_cplx.size - clamp_yf(freq_idx+epsilon)), 'constant', constant_values=0)
+
+y1f_filtered = extract_harmonic(harmonics_freq_vals[0])
+y2f_filtered = extract_harmonic(harmonics_freq_vals[1])
+y3f_filtered = extract_harmonic(harmonics_freq_vals[2])
+y4f_filtered = extract_harmonic(harmonics_freq_vals[3])
+
+y1_filtered = fft.ifft(y1f_filtered)
+y2_filtered = fft.ifft(y2f_filtered)
+y3_filtered = fft.ifft(y3f_filtered)
+y4_filtered = fft.ifft(y4f_filtered)
+
+# Исходный сигнал
+plt.figure()
+plt.plot(x, y, label='Исходный сигнал')
+plt.grid(True, alpha=0.5)
+plt.xlabel('Время, с')
+plt.ylabel('Значение функции')
+plt.title('График исходного сигнала')
+plt.legend()
+plt.show()
+
+mx_display_freq = 400
+
+# Спектр основного сигнала
+
+xf = xf[xf < mx_display_freq]
+yf = yf[:xf.size]
+
+plt.figure()
+plt.plot(xf, yf, label='Спектр исходного сигнала')
+plt.grid(True, alpha=0.5)
+plt.xlabel('Частота, гц')
+plt.ylabel('Коэффициент передачи')
+for harmonic in harmonics_freq_vals:
+    plt.axvline(harmonic, alpha=0.5, color='r', linestyle='--', label=f'{harmonic} гц')
+plt.legend()
+plt.title('Спектр исходного сигнала')
+plt.show()
+
+# Спектры
+
+plt.figure()
+plt.subplot(2, 2, 1)
+plt.plot(xf, 2.0 / N * np.abs(y1f_filtered[:xf.size]), label=f'{harmonics_freq_vals[0]} гц')
+plt.axvline(harmonics_freq_vals[0], alpha=0.5, color='r', linestyle='--')
+plt.grid(True, alpha=0.5)
+plt.xlabel('гц')
+plt.ylabel('Коэффициент передачи')
+plt.legend()
+
+plt.subplot(2, 2, 2)
+plt.plot(xf, 2.0 / N * np.abs(y2f_filtered[:xf.size]), label=f'{harmonics_freq_vals[1]} гц')
+plt.axvline(harmonics_freq_vals[1], alpha=0.5, color='r', linestyle='--')
+plt.grid(True, alpha=0.5)
+plt.xlabel('гц')
+plt.ylabel('Коэффициент передачи')
+plt.legend()
+
+plt.subplot(2, 2, 3)
+plt.plot(xf, 2.0 / N * np.abs(y3f_filtered[:xf.size]), label=f'{harmonics_freq_vals[2]} гц')
+plt.axvline(harmonics_freq_vals[2], alpha=0.5, color='r',linestyle='--')
+plt.grid(True, alpha=0.5)
+plt.xlabel('гц')
+plt.ylabel('Коэффициент передачи')
+plt.legend()
+
+plt.subplot(2, 2, 4)
+plt.plot(xf, 2.0 / N * np.abs(y4f_filtered[:xf.size]), label=f'{harmonics_freq_vals[3]} гц')
+plt.axvline(harmonics_freq_vals[3], alpha=0.5, color='r', linestyle='--')
+plt.grid(True, alpha=0.5)
+plt.xlabel('гц')
+plt.ylabel('Коэффициент передачи')
+plt.legend()
+
+plt.suptitle('Графики спектров отфильтрованных сигналов')
+plt.show()
+
+# Графики отфильтрованных сигналов
+
+# def get_signal_phase(y_filtered, freq, fs):
+#     yf = fft.fft(y_filtered)
+#     freqs = fft.fftfreq(len(y_filtered), 1/fs)
+    
+#     idx = np.argmin(np.abs(freqs - freq))
+    
+#     phase = np.angle(yf[idx])
+#     return phase
+
+# phase1 = get_signal_phase(y1_filtered, harmonics_freq_vals[0], fs)
+# phase2 = get_signal_phase(y2_filtered, harmonics_freq_vals[1], fs)
+# phase3 = get_signal_phase(y3_filtered, harmonics_freq_vals[2], fs)
+# phase4 = get_signal_phase(y4_filtered, harmonics_freq_vals[3], fs)
+
+
+plt.figure()
+plt.subplot(2, 2, 1)
+plt.plot(x[:y1_filtered.size], y1_filtered, label=f'{harmonics_freq_vals[0]} гц')
+plt.plot(x[:y1_filtered.size], np.sin(2 * np.pi * harmonics_freq_vals[0] * x[:y1_filtered.size]), alpha = 0.5, color='r', linestyle='--', label=f'Ожидаемый сигнал')
+plt.grid(True, alpha=0.5)
+plt.xlabel('Время, с')
+plt.ylabel('Значение функции')
+plt.legend()
+
+plt.subplot(2, 2, 2)
+plt.plot(x[:y2_filtered.size], y2_filtered, label=f'{harmonics_freq_vals[1]} гц')
+plt.plot(x[:y2_filtered.size], np.sin(2 * np.pi * harmonics_freq_vals[1] * x[:y1_filtered.size]), alpha = 0.5, color='r', linestyle='--', label=f'Ожидаемый сигнал')
+plt.grid(True, alpha=0.5)
+plt.xlabel('Время, с')
+plt.ylabel('Значение функции')
+plt.legend()
+
+plt.subplot(2, 2, 3)
+plt.plot(x[:y3_filtered.size], y3_filtered, label=f'{harmonics_freq_vals[2]} гц')
+plt.plot(x[:y3_filtered.size], np.sin(2 * np.pi * harmonics_freq_vals[2] * x[:y1_filtered.size]), alpha = 0.5, color='r', linestyle='--', label=f'Ожидаемый сигнал')
+plt.grid(True, alpha=0.5)
+plt.xlabel('Время, с')
+plt.ylabel('Значение функции')
+plt.legend()
+
+plt.subplot(2, 2, 4)
+plt.plot(x[:y4_filtered.size], y4_filtered, label=f'{harmonics_freq_vals[3]} гц')
+plt.plot(x[:y4_filtered.size], np.sin(2 * np.pi * harmonics_freq_vals[3] * x[:y1_filtered.size]), alpha = 0.5, color='r', linestyle='--', label=f'Ожидаемый сигнал')
+plt.grid(True, alpha=0.5)
+plt.xlabel('Время, с')
+plt.ylabel('Значение функции')
+plt.legend()
+
+plt.suptitle('Графики отфильтрованных сигналов (без коррекции амплитуд и фаз)')
+plt.show()
---
+++ hw2/hw2/hw2/hw2.py
@@ -0,0 +1,151 @@
+﻿import cv2
+import numpy as np
+from scipy.stats import pearsonr
+from skimage import util as util
+from skimage import filters as filt
+from skimage.morphology import disk, square
+from skimage import restoration
+from scipy import ndimage
+import matplotlib.pyplot as plt
+
+def img_to_uint8(img):
+    return np.uint8(img)
+
+def show_images(images_infos):
+    for name in images_infos:
+        cv2.imshow(name, images_infos[name])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def get_correlation_coeff(image1, image2):
+    coeff, _ = pearsonr(img_to_uint8(image1).flatten(), img_to_uint8(image2).flatten())
+    return coeff
+
+img = cv2.imread('./image2.jpg', 0)
+img_float32 = np.float32(img)
+
+dct_img = cv2.dct(img_float32)
+idct_img = cv2.idct(dct_img)
+
+dct_corr_coeff = get_correlation_coeff(img_float32, idct_img)
+
+show_images({'Source image' : img, 'DCT': img_to_uint8(dct_img), 'Inverse DCT': img_to_uint8(idct_img)})
+
+f_img = np.fft.fft2(img_float32)
+if_img = np.fft.ifft2(f_img)
+
+f_corr_coeff = get_correlation_coeff(img_float32, if_img)
+
+show_images({'Source image' : img, 'FFT': img_to_uint8(f_img), 'Inverse FFT': img_to_uint8(if_img)})
+
+def show_corr_coeffs(corr_coeffs):
+    for coeff in corr_coeffs:
+        print(f'{coeff}: {corr_coeffs[coeff]}')
+
+    mx_coeff = max(corr_coeffs, key=corr_coeffs.get)
+    print(f'Наиболее точный результат: {mx_coeff}')
+
+
+corr_coeffs = {'Косинусное преобразование': dct_corr_coeff, 'Преобразование Фурье': f_corr_coeff}
+show_corr_coeffs(corr_coeffs)
+
+# print(f'Коэфф. корреляции косинусного преобразования: {dct_corr_coeff}\nКоэфф. корреляции преобразования Фурье: {f_corr_coeff}')
+# if dct_corr_coeff > f_corr_coeff:
+#     print ('Результат косинусного преобразования более точен!')
+# else:
+#     print ('Результат преобразования Фурье более точен!')
+
+gaussian_img = util.random_noise(img, mode='gaussian', var=0.01)
+salt_and_pepper_img = util.random_noise(img, mode='s&p', amount=0.3)
+speckle_img = util.random_noise(img, mode='speckle',var=0.01)
+
+mask_size = 3
+
+def get_psf(ds):
+    psf = ndimage.gaussian_filter(np.ones((int(ds), int(ds))), sigma=1.1)
+    psf = psf / psf.sum() 
+    return psf
+
+balance = 0.05
+
+median_img = filt.median(gaussian_img, footprint=disk(mask_size))
+rank_img = filt.rank.median(salt_and_pepper_img, footprint=disk(mask_size))
+wiener_img = restoration.wiener(speckle_img, get_psf(mask_size), balance=balance)
+
+show_images({'Source image' : img, 'Gaussian noise': gaussian_img, 'S&P noise': salt_and_pepper_img, 'Speckle noise': speckle_img})
+show_images({'Source image' : img, 'Median filter': median_img, 'Rank filter': rank_img, 'Wiener filter': wiener_img})
+
+median_corr_coeff = get_correlation_coeff(img_float32, median_img)
+rank_corr_coeff = get_correlation_coeff(img_float32, rank_img)
+wiener_corr_coeff  = get_correlation_coeff(img_float32, wiener_img)
+
+corr_coeffs = {'Медианный фильтр': median_corr_coeff, 'Ранговый фильтр': rank_corr_coeff, 'Фильтр Винера': wiener_corr_coeff}
+show_corr_coeffs(corr_coeffs)
+
+masks = [1, 3, 7]
+for mask in masks:
+    print(f"Фильтрация, размер маски = {mask}")
+    median_img = filt.median(gaussian_img, footprint=disk(mask))
+    rank_img = filt.rank.median(salt_and_pepper_img, footprint=disk(mask))
+    wiener_img = restoration.wiener(speckle_img, get_psf(mask), balance=balance)
+    show_images({'Source image' : img, f'Median filter (mask = {mask})': median_img, f'Rank filter (mask = {mask})': rank_img, f'Wiener filter (mask = {mask})': wiener_img})
+
+
+mask_sizes = np.arange(1, 19, 1)
+
+median_coeffs = [get_correlation_coeff(img_float32, filt.median(gaussian_img, footprint=disk(i))) for i in mask_sizes]
+rank_coeffs = [get_correlation_coeff(img_float32, filt.rank.median(salt_and_pepper_img, footprint=disk(i))) for i in mask_sizes]
+wiener_coeffs = [get_correlation_coeff(img_float32, restoration.wiener(speckle_img, get_psf(i), balance=balance)) for i in mask_sizes]
+
+plt.figure()
+plt.subplot(2,2,1)
+plt.plot(mask_sizes, median_coeffs, 'o-', label='Медианная фильтрация')
+plt.xlabel('Размер маски')
+plt.ylabel('Коэффициент корреляции')
+plt.grid(True, alpha=0.5)
+plt.ylim(-1.0, 1.1)
+plt.axhline(y=0.0, color='black', linestyle='--')
+plt.legend()
+plt.title('Медианная фильтрация')
+
+plt.subplot(2,2,2)
+plt.plot(mask_sizes, rank_coeffs, 'o-', label='Ранговая фильтрация')
+plt.xlabel('Размер маски')
+plt.ylabel('Коэффициент корреляции')
+plt.grid(True, alpha=0.5)
+plt.ylim(-1.0, 1.1)
+plt.axhline(y=0.0, color='black', linestyle='--')
+plt.legend()
+plt.title('Ранговая фильтрация')
+
+plt.subplot(2,2,3)
+plt.plot(mask_sizes, wiener_coeffs, 'o-', label='Фильтрация Винера')
+plt.xlabel('Размер маски')
+plt.ylabel('Коэффициент корреляции')
+plt.grid(True, alpha=0.5)
+plt.ylim(-1.0, 1.1)
+plt.axhline(y=0.0, color='black', linestyle='--')
+plt.legend()
+plt.title('Фильтрация Винера')
+
+plt.show()
+
+f_img = 10 * np.log(np.abs(np.fft.fftshift(f_img)) + 1)
+rows, cols = f_img.shape
+
+center_x, center_y = cols // 2, rows // 2
+y, x = np.indices((rows, cols))
+r = np.sqrt((x - center_x)**2 + (y - center_y)**2)
+r = r.astype(int)
+    
+tbin = np.bincount(r.ravel(), f_img.ravel())
+nr = np.bincount(r.ravel())
+radial_profile = tbin / nr
+    
+plt.plot(radial_profile[:min(rows, cols)//2])
+plt.title('Зависимость спектра яркости от частоты')
+plt.xlabel('Радиус (пиксели)')
+plt.ylabel('Амплитуда')
+plt.grid(True, alpha=0.5)
+    
+plt.show()
